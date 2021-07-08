06060567bb49231de0ee0ff1190879b6241eb17c
Jojorik <egor31245@gmail.com>
2021-07-08 10:56:37 +0300

    Создан стартовый шаблон проекта.

---
+++ hosp6/src/main/java/com/example/hosp6/controllers/MainController.java
@@ -0,0 +1,18 @@
+/*package com.example.hosp6.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("title", "Старт Spring");
+        return "home";
+    }
+}
+*/
---
+++ hosp6/src/test/java/com/example/hosp6/Hosp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hosp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hosp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
